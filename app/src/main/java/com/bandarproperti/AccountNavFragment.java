9aa90dce3ff1881a35fb38484e54e29dd72d1f79
fikrikurniawanl <fikrikurniawan1176@gmail.com>
2019-11-12 00:06:29 +0700

    Tambah Edit Profil (Button Error)

--- app/src/main/java/com/bandarproperti/AccountNavFragment.java
+++ app/src/main/java/com/bandarproperti/AccountNavFragment.java
@@ -1,12 +1,14 @@
 package com.bandarproperti;
 
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 
 public class AccountNavFragment extends Fragment {
@@ -21,6 +23,7 @@ public class AccountNavFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.activity_profil, container, false);
         return view;
+
     }
 
 }
